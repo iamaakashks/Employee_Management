53e35c21bc088185873bca0cd47c6e82d3e3134f
Aakash Kumar Suman <121715040+iamaakashks@users.noreply.github.com>
2024-10-20 05:09:13 +0530

    taskname updated

--- src/Components/others/TaskListNumbers.jsx
+++ src/Components/others/TaskListNumbers.jsx
@@ -3,19 +3,19 @@ import React from "react";
 const TaskListNumbers = () => {
   return (
     <div className='w-full px-24 flex gap-8 mt-8'>
-        <div className="w-full flex flex-col gap-4 bg-red-400 py-4 px-8 rounded-md">
+        <div className="w-full flex flex-col gap-4 bg-gradient-to-r from-blue-400 to-purple-400 py-4 px-8 rounded-md">
             <h1 className="font-medium text-4xl">0</h1>
             <h4 className="font-bold text-xl">New Task</h4>
         </div>
-        <div className="w-full flex flex-col gap-4 bg-red-400 py-4 px-8 rounded-md">
+        <div className="w-full flex flex-col gap-4 bg-gradient-to-r from-blue-400 to-purple-400 py-4 px-8 rounded-md">
             <h1 className="font-medium text-4xl">0</h1>
             <h4 className="font-bold text-xl">Completed</h4>
         </div>
-        <div className="w-full flex flex-col gap-4 bg-red-400 py-4 px-8 rounded-md">
+        <div className="w-full flex flex-col gap-4 bg-gradient-to-r from-blue-400 to-purple-400 py-4 px-8 rounded-md">
             <h1 className="font-medium text-4xl">0</h1>
             <h4 className="font-bold text-xl">Accepted</h4>
         </div>
-        <div className="w-full flex flex-col gap-4 bg-red-400 py-4 px-8 rounded-md">
+        <div className="w-full flex flex-col gap-4 bg-gradient-to-r from-blue-400 to-purple-400 py-4 px-8 rounded-md">
             <h1 className="font-medium text-4xl">0</h1>
             <h4 className="font-bold text-xl">Failed</h4>
         </div>
