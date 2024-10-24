0c93edcf1628d54bc4646697e2fbbb5dcae56649
Aakash Kumar Suman <121715040+iamaakashks@users.noreply.github.com>
2024-10-24 17:11:09 +0530

    login as employee and admin has been setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,16 @@ import AdminDashboard from "./Components/Dashboard/AdminDashboard.jsx";
 import { AuthContext } from "./context/AuthProvider.jsx";
 const App = () => {
   const [user, setUser] = useState(null);
-  
+  const authData = useContext(AuthContext);
   const handleLogin = (email, password)=>{
-    console.log(console.log(authData.employee.find((e)=> email == e.email && password == e.password)))
-    console.log("hello");
-    if(email === "admin@me.com" && password == "123"){
+    if(authData && authData.admin.find((e)=>email == e.email && password == e.password)){
       setUser("admin")
-    }else if(email == "user@me.com" && password == "123"){
+    }else if(authData && authData.employee.find((e)=> email == e.email && password == e.password)){
       setUser("employee")
     }else{
       alert("invalid Credentials");
     }
   }
-
-  const authData = useContext(AuthContext);
   return (
     <div className="bg-zinc-900 w-full text-zinc-50">
       {!user? <Login handleLogin={handleLogin}/> : ""}
