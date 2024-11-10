df34ca35b9226976ab074b49ac75cac93a99b9b2
Aakash Kumar Suman <121715040+iamaakashks@users.noreply.github.com>
2024-11-10 23:55:45 +0530

    fixed the padding in tasklist in employeeDashboard

--- src/Components/TaskList/NewTask.jsx
+++ src/Components/TaskList/NewTask.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NewTask = ({data}) => {
   return (
-    <div className="flex-shrink-0 w-1/4 h-full border-[1px] border-zinc-800 rounded-xl p-6">
+    <div className="flex-shrink-0 w-1/4 h-full border-[1px] border-zinc-800 rounded-xl p-4">
         <div className="flex items-center justify-between">
             <h1 className="px-2.5 py-0.5 rounded-md border-[1px] bg-slate-600">{data.category}</h1>
             <h4 className="text-sm">{data.date}</h4>
